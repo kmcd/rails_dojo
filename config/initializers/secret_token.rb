5c23f1eb22047747e9d90659707215211edd1004
Keith Mc Donnell <keith@dancingtext.com>
2011-04-29 14:06:44 +0100

    Starting

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsdojo::Application.config.secret_token = 'ce2e3306d4cc0179fbc6f7804c879328cea4a5a690c94723d79b38304e70fc27eeef8b6862ce516f5481fb4270cd5c37e9a7b42a5cd1394bb5863809e6434162'
